e00bbb2d5b8d16d6737a56eca02e71ca5aca08bf
AdrianFreemantle <adrian@synerics.com>
2013-11-19 16:10:17 +0200

    Added additional nuspec file

--- Packaging/package.bat
+++ Packaging/package.bat
@@ -2,6 +2,7 @@ rd /S /Q Packages
 mkdir Packages
 
 nuget pack Hermes.Core.nuspec
+nuget pack Hermes.Messaging.nuspec
 nuget pack Hermes.EntityFramework.nuspec
 nuget pack Hermes.SeriviceStack.nuspec
 nuget pack Hermes.ServiceHost.nuspec
