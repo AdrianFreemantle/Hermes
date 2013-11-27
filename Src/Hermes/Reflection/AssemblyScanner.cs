22af7091467fab7718600baa5f6445bb951e3ff1
AdrianFreemantle <adrian@synerics.com>
2013-11-27 11:11:39 +0200

    fixed assembly scanner rules

--- Src/Hermes/Reflection/AssemblyScanner.cs
+++ Src/Hermes/Reflection/AssemblyScanner.cs
@@ -8,6 +8,28 @@ using Hermes.Logging;
 
 namespace Hermes.Reflection
 {
+    public static class AssemblyScannerDefaultIgnoreRules
+    {
+        public static List<Func<string, bool>> Rules { get; private set; }
+
+        static AssemblyScannerDefaultIgnoreRules()
+        {
+            Rules.AddRange(new Func<string, bool>[]
+            {
+                s => s.StartsWith("Microsoft.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.StartsWith("ServiceStack.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.StartsWith("ServiceStack.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.StartsWith("Newtonsoft.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.StartsWith("System.", StringComparison.CurrentCultureIgnoreCase), 
+                s => s.StartsWith("Autofac.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.Equals("Antlr3.Runtime.dll", StringComparison.CurrentCultureIgnoreCase),
+                s => s.Equals("EntityFramework.dll", StringComparison.CurrentCultureIgnoreCase),
+                s => s.Equals("WebGrease.dll", StringComparison.CurrentCultureIgnoreCase),
+                s => s.Equals("gsdll32.dll", StringComparison.CurrentCultureIgnoreCase)
+            });
+        }
+    }
+
     public class AssemblyScanner : IDisposable
     {
         private static readonly List<Func<string, bool>> ExclusionRules = new List<Func<string, bool>>();
@@ -22,7 +44,25 @@ namespace Hermes.Reflection
 
         public AssemblyScanner()
         {
+            AssemblyScannerDefaultIgnoreRules();
             Scan();
+        }
+
+        private static void AssemblyScannerDefaultIgnoreRules()
+        {
+            ExclusionRules.AddRange(new Func<string, bool>[]
+            {
+                s => s.StartsWith("Microsoft.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.StartsWith("ServiceStack.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.StartsWith("ServiceStack.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.StartsWith("Newtonsoft.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.StartsWith("System.", StringComparison.CurrentCultureIgnoreCase), 
+                s => s.StartsWith("Autofac.", StringComparison.CurrentCultureIgnoreCase),
+                s => s.Equals("Antlr3.Runtime.dll", StringComparison.CurrentCultureIgnoreCase),
+                s => s.Equals("EntityFramework.dll", StringComparison.CurrentCultureIgnoreCase),
+                s => s.Equals("WebGrease.dll", StringComparison.CurrentCultureIgnoreCase),
+                s => s.Equals("gsdll32.dll", StringComparison.CurrentCultureIgnoreCase)
+            });
         }
 
         public static void Exclude(Func<string, bool> exclusionRule)
@@ -39,7 +79,7 @@ namespace Hermes.Reflection
 
             var assemblyFiles = baseDirectory.GetFiles("*.dll", SearchOption.AllDirectories)
                                              .Union(baseDirectory.GetFiles("*.exe", SearchOption.AllDirectories))
-                                             .Where(info => ExclusionRules.All(func => func(info.FullName)));
+                                             .Where(info => ExclusionRules.All(func => !func(info.Name)));
 
             foreach (var assemblyFile in assemblyFiles)
             {
