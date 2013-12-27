1ed49aa361a5797e822e0af2bead9b560e657e6d
AdrianFreemantle <adrian@synerics.com>
2013-12-27 16:22:38 +0200

    Added missing files

---
+++ Src/Hermes.Messaging/Transports/OutgoingMessageUnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+using Hermes.Logging;
+using Hermes.Messaging.Pipeline;
+using Hermes.Pipes;
+
+namespace Hermes.Messaging.Transports
+{
+    public class OutgoingMessageUnitOfWork
+    {
+        private static readonly ILog Logger = LogFactory.BuildLogger(typeof(OutgoingMessageUnitOfWork)); 
+
+        private readonly Queue<OutgoingMessageContext> outgoingMessages = new Queue<OutgoingMessageContext>();
+        private readonly ModuleStack<OutgoingMessageContext> outgoingPipeline;
+
+        public OutgoingMessageUnitOfWork(ModuleStack<OutgoingMessageContext> outgoingPipeline)
+        {
+            this.outgoingPipeline = outgoingPipeline;
+        }
+
+        public void Enqueue(OutgoingMessageContext outgoingMessageContext)
+        {
+            Logger.Debug("Enqueuing outgoing message {0}", outgoingMessageContext);
+            outgoingMessages.Enqueue(outgoingMessageContext);
+        }
+
+        public void Commit()
+        {            
+            while (outgoingMessages.Count > 0)
+            {
+                OutgoingMessageContext outgoingContext = outgoingMessages.Dequeue();
+                Logger.Debug("Sending enqueued message {0}", outgoingContext);
+                outgoingContext.Process(outgoingPipeline, Ioc.ServiceLocator.Current);
+            }
+        }
+
+        public void Clear()
+        {
+            outgoingMessages.Clear();
+        }
+    }
+}
---
+++ Src/Hermes.ServiceHost/HostableService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using Topshelf;
+
+namespace Hermes.ServiceHost
+{
+    public class HostableService
+    {
+        private readonly Type hostableService;
+
+        public HostableService(Type hostableService)
+        {
+            this.hostableService = hostableService;
+        }
+
+        public TopshelfExitCode Run()
+        {
+            var host = Topshelf.HostFactory.New(configurator =>
+            {
+                configurator.SetServiceName(GetServiceName());
+                configurator.SetDisplayName(GetServiceName());
+                configurator.SetDescription(GetDescription());
+                configurator.RunAsPrompt();
+
+                configurator.Service<ServiceHost>(s =>
+                {
+                    s.ConstructUsing(() => new ServiceHost(hostableService));
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+            });
+
+            return host.Run();
+        }
+
+        private string GetDescription()
+        {
+            var descriptionAttribute = hostableService
+                .Assembly
+                .GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false)
+                .OfType<AssemblyDescriptionAttribute>()
+                .FirstOrDefault();
+
+            return descriptionAttribute != null ? descriptionAttribute.Description : GetServiceName();
+        }
+
+        public string GetServiceFilePath()
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, hostableService.Assembly.ManifestModule.Name);
+        }
+
+        public string GetServiceName()
+        {
+            return String.Format("Hermes.{0}", hostableService.Assembly.GetName().Name);
+        }
+
+        public string GetConfigurationFilePath()
+        {
+            return GetServiceFilePath() + ".config";
+        }
+    }
+}
