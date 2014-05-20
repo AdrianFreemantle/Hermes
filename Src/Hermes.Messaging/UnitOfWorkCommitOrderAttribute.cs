6249c56503d81bbaf4070c3e1458b89600249be4
Adrian Freemantle <adrian@synerics.com>
2014-05-20 11:39:01 +0200

    added missing files

---
+++ Src/Hermes.Messaging/UnitOfWorkCommitOrderAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Hermes.Messaging
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class UnitOfWorkCommitOrderAttribute : Attribute
+    {
+        public int Order { get; set; }
+    }
+}
