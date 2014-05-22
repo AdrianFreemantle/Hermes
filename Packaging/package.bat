1a0f64e6ce36147342e817876c9972feeda173f3
Adrian Freemantle <adrian@synerics.com>
2014-05-22 07:47:25 +0200

    updaed nuspec files

--- Packaging/package.bat
+++ Packaging/package.bat
@@ -3,7 +3,6 @@ set var=0.5.43-test
 d:\LocalNugetPackages\nuget pack Hermes.Core.nuspec -Version %var%
 d:\LocalNugetPackages\nuget pack Hermes.Messaging.nuspec -Version %var%
 d:\LocalNugetPackages\nuget pack Hermes.EntityFramework.nuspec -Version %var%
-d:\LocalNugetPackages\nuget pack Hermes.OrmLite.nuspec -Version %var%
 d:\LocalNugetPackages\nuget pack Hermes.ServiceHost.nuspec -Version %var%
 
 move *.nupkg D:\\LocalNugetPackages
