23b9ee0aa37f4b323e2040f0e257d40ba214ecdd
Adrian Freemantle <adrian@synerics.com>
2014-10-14 10:31:06 +0200

    fixed bug with the FirstOrCreate and SingleOrCreate Methods on DbSetExtensions

--- Src/Hermes.EntityFramework/DbSetExtensions.cs
+++ Src/Hermes.EntityFramework/DbSetExtensions.cs
@@ -13,12 +13,18 @@ namespace Hermes.EntityFramework
 
         public static TEntity FirstOrCreate<TEntity>(this IDbSet<TEntity> source, Func<TEntity, bool> predicate) where TEntity : class
         {
-            return source.Local.FirstOrDefault(predicate) ?? source.FirstOrDefault(predicate) ?? source.Create();
+            return source.Local.FirstOrDefault(predicate) ?? source.FirstOrDefault(predicate) ?? CreateEntity(source);
         }
-
+        
         public static TEntity SingleOrCreate<TEntity>(this IDbSet<TEntity> source, Func<TEntity, bool> predicate) where TEntity : class
         {
-            return source.Local.SingleOrDefault(predicate) ?? source.SingleOrDefault(predicate) ?? source.Create();
+            return source.Local.SingleOrDefault(predicate) ?? source.SingleOrDefault(predicate) ?? CreateEntity(source);
+        }
+
+        private static TEntity CreateEntity<TEntity>(IDbSet<TEntity> source) where TEntity : class
+        {
+            var entity = source.Create();
+            return source.Attach(entity);
         }
     }
 }
