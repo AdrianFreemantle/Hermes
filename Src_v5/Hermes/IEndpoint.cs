269b26054a04af822d541a50643ba6a0f6ef6a7f
Adrian Freemantle <adrian@synerics.com>
2015-10-13 17:02:56 +0200

    migrating existing code to V5

--- Src_v5/Hermes/IEndpoint.cs
+++ Src_v5/Hermes/IEndpoint.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Threading;
+using Hermes.Ioc;
 
 namespace Hermes
 {
     public interface IEndpoint : IDisposable
     {
+        IEndpoint RegisterDependencies<T>() where T : IRegisterDependencies, new();
         void Start(CancellationToken token);
     }
 }
